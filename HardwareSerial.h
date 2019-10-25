f21c9f62f813378c154396473e8c482129e38059
Jens Willy Johannsen <jens@greenerpastures.dk>
2019-10-25 14:58:50 +0200

    Changes to abstract HardwareSerial class

--- HardwareSerial.h
+++ HardwareSerial.h
@@ -15,23 +15,25 @@ enum e_SerialMode
 	SERIAL_8E1
 };
 
+/**
+ * Abstract (mostly, anyway) serial interface class.
+ *
+ * Implement all the virtual functions in
+ */
 class HardwareSerial
 {
 public:
 	HardwareSerial();
 	~HardwareSerial();
 
-	virtual int available() { return 0; }
-    virtual int peek() { return 0; }
-	virtual uint8_t read() { return 0; }
+	virtual int available() = 0;
+	virtual uint8_t read() = 0;
 
-    virtual void begin( unsigned long baud, e_SerialMode mode ) {}
-	virtual void end() {}
+    virtual void begin( unsigned long baud, e_SerialMode mode ) = 0;
+	virtual void end() = 0;
 
-    virtual int write( uint8_t data ) { return 1; };
-    virtual void flush() {}
-
-    int write( uint8_t *buffer, int size );
+    virtual int write( uint8_t data ) = 0;
+    virtual int write( uint8_t *buffer, int size );	// This method has a default implementation calling write(uint8_t)
 };
 
 #endif /* KNXDEVICE_HARDWARESERIAL_H_ */
